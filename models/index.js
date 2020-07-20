ff29278ff660bf1fc959154451fe26d2e524f501
Terik Hone <terikhone@gmail.com>
2020-07-20 13:49:36 -0600

    created question with answers model

--- models/index.js
+++ models/index.js
@@ -1,5 +1,5 @@
 // Exporting an object containing all of our models
 module.exports = {
   User: require("./user.model"),
-
+  Post: require("./posts.model"),
 };
--- models/posts.model.js
+++ models/posts.model.js
@@ -2,7 +2,6 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const postSchema = new Schema({
-  name: String,
   author: { type: Schema.Types.ObjectID, ref: 'User' },
   content: String,
   visibleTo: String,
