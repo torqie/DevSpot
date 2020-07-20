f95e4f5efb015e7b9f25e5dc5b72662339c25c3f
Terik Hone <terikhone@gmail.com>
2020-07-20 13:39:26 -0600

    updated posts model with comments

--- models/posts.model.js
+++ models/posts.model.js
@@ -6,6 +6,10 @@ const postSchema = new Schema({
   author: { type: Schema.Types.ObjectID, ref: 'User' },
   content: String,
   visibleTo: String,
+  comments: [{
+    author: { type: Schema.Types.ObjectID, ref: 'User' },
+    content: String
+  }]
 });
 
 const Post = mongoose.model("Post", postSchema);
