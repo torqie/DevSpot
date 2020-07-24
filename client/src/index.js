2b7be98d3fcbb83d7ddb31cb714a18f5be0ba9c9
Terik Hone <terikhone@gmail.com>
2020-07-23 22:25:28 -0600

    removed some unneeded files

--- client/src/index.js
+++ client/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import registerServiceWorker from "./registerServiceWorker";
-import './customBootstrap.scss';
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
