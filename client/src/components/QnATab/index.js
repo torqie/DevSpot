f054adab2df3728ccfa597bdee5405fb9b67e6a5
jonmeidell <jonmeidell@gmail.com>
2020-07-28 18:58:11 -0600

    intial work on posting questions to server

--- client/src/components/QnATab/index.js
+++ client/src/components/QnATab/index.js
@@ -3,14 +3,55 @@ import axios from 'axios'
 import "./style.less";
 
 class NewQuestionsCard extends Component {
+
 	state = {
 		loading: true,
-		article: {}
-	}
+		article: {},
+		showPostButton: false,
+		posts: []
+	};
+
+	onChange = value => {
+		if (value.length > 0) {
+			this.setState({ questionText: value, showPostButton: true })
+		} else {
+			this.setState({ questionText: value, showPostButton: false })
+		}
+	};
+
+	onSubmit = async () => {
+		const currentUser = await axios.get(`/api/users/${localStorage.getItem("userId")}`);
+		this.setState({ loading: true });
+		axios
+			.post('/api/questions', {
+				author: currentUser.data,
+				content: this.state.questionText,
+				visibleTo: "public"
+			}).then(response => {
+				console.log(response);
+				this.loadQuestions();
+				this.updateQuestionCount();
+				this.setState({ loading: false, questionText: '' });
+			})
+			.catch(error => {
+				console.log("Error posting new question: ", error);
+			});
+	};
+
+  loadQuestions = () => {
+    axios
+      .get('/api/questions').then(response => {
+        console.log("questions: ", response.data);
+        this.setState({questions: response.data});
+      })
+      .catch(error => {
+        console.log("Error retrieving questions: ", error);
+    })
+  };
 
 	componentDidMount() {
 		this.setState({ loading: false })
-	}
+	};
 
 	render() {
 		return (
@@ -27,5 +68,7 @@ class NewQuestionsCard extends Component {
 		);
 	}
 }
+
+
 
 export default NewQuestionsCard;
