1e30db948c043cae68e3d25af06a2eb7e8164684
jonmeidell <jonmeidell@gmail.com>
2020-07-28 21:07:46 -0600

    changed layout to antd

--- client/src/components/QnATab/index.js
+++ client/src/components/QnATab/index.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react'
+import { Row, Col, Form, Input, Button, Avatar, Empty } from "antd";
+import NewsFeedCard from "../NewsFeedCard";
 import axios from 'axios'
 import "./style.less";
 
@@ -6,16 +8,22 @@ class NewQuestionsCard extends Component {
 
 	state = {
 		loading: true,
-		article: {},
-		showPostButton: false,
+		showQuestionButton: false,
+		updateText: '',
 		questions: []
 	};
 
 	onChange = value => {
 		if (value.length > 0) {
-			this.setState({ questionText: value, showPostButton: true })
+			this.setState({
+				questionText: value, showQuestionButton
+					: true
+			})
 		} else {
-			this.setState({ questionText: value, showPostButton: false })
+			this.setState({
+				questionText: value, showQuestionButton
+					: false
+			})
 		}
 	};
 
@@ -38,16 +46,16 @@ class NewQuestionsCard extends Component {
 			});
 	};
 
-  loadQuestions = () => {
-    axios
-      .get('/api/questions').then(response => {
-        console.log("questions: ", response.data);
-        this.setState({questions: response.data});
-      })
-      .catch(error => {
-        console.log("Error retrieving questions: ", error);
-    })
-  };
+	loadQuestions = () => {
+		axios
+			.get('/api/questions').then(response => {
+				console.log("questions: ", response.data);
+				this.setState({ questions: response.data });
+			})
+			.catch(error => {
+				console.log("Error retrieving questions: ", error);
+			})
+	};
 
 	componentDidMount() {
 		this.setState({ loading: false })
@@ -55,16 +63,59 @@ class NewQuestionsCard extends Component {
 
 	render() {
 		return (
-			<div>
-				<form class="qna-body field">
-					<textarea id="qna-text" rows="5" type="text" placeholder="Ask you question here..."></textarea>
-				</form>
+			<>
+				<Form>
+					<Row>
+						<Input
+							autoSize
+							style={{ width: '100%', border: 0 }}
+							onChange={this.onChange}
+							onSearch={this.onSearch}
+							onSelect={this.onSelect}
+							placeholder="Ask you question here..."
+							value={this.state.questionText}
+						>
+							{this.state.users.length > 0 ? (
+								this.state.users.map((user, index) => {
+									return (
+										<option key={user._id} value={user.name}>
+											<Avatar src={user.avatar} style={{ marginRight: "4px" }} />{user.name}
+										</option>
+									)
+								})
+							) : null}
+						</Input>
+					</Row>
 
-				<div class="qna-button">
-					<button type="submit" class="qna-submit">Submit question</button>
-				</div>
+					{this.state.showQuestionButton &&
+						<Row style={{ marginTop: "10px" }}>
+							<Col span={24} style={{ textAlign: 'right' }}>
+								<Button type="primary" htmlType="submit" onClick={this.onSubmit}>
+									Ask Question
+                </Button>
+							</Col>
+						</Row>
+					}
+				</Form>
+				{this.state.questions.length > 0 ? (
+					this.state.questions.map((question, index) => {
+						return <NewsFeedCard key={index} author={question.author} content={question.content} visibleTo={question.visibleTo} />
+					})
+				) : <Empty />}
 
-			</div>
+			</>
+
+			// <div>
+			// 	<form class="qna-body field">
+			// 		<textarea id="qna-text" rows="5" type="text" placeholder="Ask you question here..."></textarea>
+			// 	</form>
+
+			// 	<div class="qna-button">
+			// 		<button type="submit" class="qna-submit">Submit question</button>
+			// 	</div>
+
+			// </div>
+
 		);
 	}
 }
