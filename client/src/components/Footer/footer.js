cd21485e4ffab7c7fa233b369b98c4fe21a12dfa
Justin <justinj.lambeth@gmail.com>
2020-07-30 20:03:37 -0600

    needing correct css for footer

--- client/src/components/Footer/footer.js
+++ client/src/components/Footer/footer.js
@@ -1,14 +1,56 @@
-import React from "react" 
+import React from "react"
+import "./Footer.css"
 
-function Footer({ children }) {
+const Footer = () => {
     return (
-        <section className="rc-footer-container">
-            <section className="rc-footer-columns">
-                
-            </section>
+        <div className="main-footer">
+            <div className="container">
+                <div className="row">
+                    {/* Column1 */}
+                    <div className="col">
+                        <h4>
+                            place holder
+                        </h4>
+                        <ul className="list-unstyled">
+                            <li>111</li>
+                            <li>222</li>
+                            <li>3333</li>
+                        </ul>
+                    </div>
+                    {/* Column2 */}
+                    <div className="col">
+                        <h4>
+                            place holder
+                        </h4>
+                        <ul className="list-unstyled">
+                            <li>111</li>
+                            <li>222</li>
+                            <li>3333</li>
+                        </ul>
+                    </div>
+                    {/* Column3 */}                    
+                    <div className="col">
+                        <h4>
+                            place holder
+                        </h4>
+                        <ul className="list-unstyled">
+                            <li>111</li>
+                            <li>222</li>
+                            <li>3333</li>
+                        </ul>
+                    </div>
+                </div>
+                <hr />
+                <div className="row">
+                    <p className="col-sm">
+                        &copy;{new Date().getFullYear()} Devspot | All rights reserved | Terms of service | Privacy
+                    </p>
+                </div>
+            </div>
 
-        </section>
-        
+
+        </div>
+
     )
 }
 
--- client/src/layouts/main2/index.js
+++ client/src/layouts/main2/index.js
@@ -2,8 +2,8 @@ import { Layout, Menu } from "antd";
 import React, { Component } from "react";
 import "./style.css";
 import Navigation from "../../components/Navigation";
-import Footer from "../../components/Footer/footer.js";
-const { Header, Content} = Layout;
+import Footer from "../../components/Footer/footer";
+const { Header, Content } = Layout;
 
 
 class MainLayout2 extends Component {
@@ -19,26 +19,32 @@ class MainLayout2 extends Component {
 
   render() {
     return (
-        <Layout theme={this.state.theme} style={{minHeight: "100vh"}}>
-            <Header theme="dark">
-              <div className="logo">
-                <h3>DevSpot</h3>
-              </div>
-              <Navigation />
-            </Header>
-            <Content
-                className="site-layout"
-                style={{
-                  marginTop: 40,
-                  padding: '0 40px',
-                  minHeight: 280,
-                }}
+      <Layout theme={this.state.theme} style={{ minHeight: "100vh" }}>
+        <Header theme="dark">
+          <div className="logo">
+            <h3>DevSpot</h3>
+          </div>
+          <Navigation />
+        </Header>
+        <Content
+          className="site-layout"
+          style={{
+            marginTop: 40,
+            padding: '0 40px',
+            minHeight: 280,
+          }}
+        >
 
-            >
-              {this.props.children}
-            </Content>
-          <Footer  style={{ textAlign: 'center' }}></Footer>
-          </Layout>
+          {this.props.children}
+        </Content>
+
+        <div className="page-container">
+          <div className="content-wrap">
+            <Footer />
+          </div>
+        </div>
+
+      </Layout>
     );
   }
 }
