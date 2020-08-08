d1cc833b7eb8596b317f883e9e7a6bed297da527
Terik Hone <terikhone@gmail.com>
2020-08-07 23:42:02 -0600

    fixing heroku issue

---
+++ build/precache-manifest.f3683b8ebd7e384e787c2810f9f0b9f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dbe9bbfbcedc4d6fd3f506362f4fd175",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e440c96a50165aa815c5",
+    "url": "/static/css/main.b817fe13.chunk.css"
+  },
+  {
+    "revision": "a7948533ccabd834af05",
+    "url": "/static/js/2.c55b5506.chunk.js"
+  },
+  {
+    "revision": "2069192f75c6f3fb6247231eea52f248",
+    "url": "/static/js/2.c55b5506.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e440c96a50165aa815c5",
+    "url": "/static/js/main.c1319b89.chunk.js"
+  },
+  {
+    "revision": "e2c6e531b8324ac17d6c",
+    "url": "/static/js/runtime-main.206ac5b9.js"
+  }
+]);
