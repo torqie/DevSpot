c39dd4d4531c08288299031a6420ea45e5cd6465
Terik Hone <terikhone@gmail.com>
2020-08-07 23:08:59 -0600

    removed files that got pushed to the master

--- controllers/auth.controller.js
+++
@@ -1,7 +0,0 @@
-const db = require("../models");
-const mongoose = require("mongoose");
-// Get all users.
-exports.all = async (req, res) => {
-  const users = await db.User.find();
-  return res.json(users);
-};
